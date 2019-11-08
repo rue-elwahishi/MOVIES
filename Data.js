95af5b0d8680b600f783cb9ddb710c2e11483fe8
rue-elwahishi <rubawahishi@gmail.com>
2019-11-08 11:01:58 +0100

    phase 3

---
+++ Data.js
@@ -0,0 +1,192 @@
+var movies = [
+       
+          { 
+       	title : 'The Shining',
+       	director : 'Stanley Kubrick',
+       	img : "images/TheShining.jpg",
+       	genre : "Classic",
+       }, 
+
+       { 
+       	title : "one flew over the cukoos nest",
+       	director : "Miloš Forman",
+       	img : "images/cuckoos.jpg",
+       	genre : "Classic",
+       },
+
+          { 
+       	title : "pulp fiction",
+       	director : "Quentin Tarantino",
+       	img : "images/pulpfiction.jpg",
+       	genre : "Classic",
+       },
+
+
+       { 
+       	title : "saving private Ryan",
+       	director : "Steven Spielberg",
+       	img : "images/savingPR.jpg",
+       	genre : "Classic",
+       },
+        { 
+       	title : "psycho",
+       	director : "Hitchcock",
+       	img : "images/psycho.jpg",
+       	genre : "Classic",
+       },
+        
+        { 
+       	title : "The Godfather",
+       	director : "Francis Ford Coppola",
+       	img : "images/TheGodfather.jpg",
+       	genre : "Classic",
+       },
+
+        { 
+       	title : "Taxi Driver",
+       	director : "Martin Scorsese",
+       	img : "images/Taxi_driver_movieposter.jpg",
+       	genre : "Classic",
+       },
+    
+
+
+        { 
+       	title : "Avengers",
+       	director : "Anthony Russo",
+       	img : "images/Avengers.jpg",
+       	genre : "Sci-fi",
+       },
+
+        { 
+       	title : "interstellar",
+       	director : "Christopher Nolan",
+       	img : "images/interstellar.jpg",
+       	genre : "Sci-fi",
+       },
+ { 
+       	title : "comet",
+       	director : "Sam Esmail",
+       	img : "images/comet.jpg",
+       	genre : "Sci-fi",
+       },
+
+         { 
+       	title : "Star Wars RogueOne",
+       	director : "J.J. Abrams",
+       	img : "images/RogueOne.jpg",
+       	genre : "Sci-fi",
+       },
+
+  { 
+       	title : "inception",
+       	director : "Christopher Nolan",
+       	img : "images/inception.jpg",
+       	genre : "Sci-fi",
+       },
+
+
+         { 
+       	title : "Million Dollar Baby",
+       	director : "Clint Eastwood",
+       	img : "images/MillionDollarBaby.jpg",
+       	genre : "Drama",
+       },
+
+
+          { 
+       	title : "manchester by the sea",
+       	director : "Kenneth Lonergan",
+       	img : "images/Manchester.jpg",
+       	genre : "Drama",
+       },
+
+
+        { 
+       	title : "Spider-Man far from home",
+       	director : "Jon Watts",
+       	img :"images/FFH.jpg",
+       	genre : "Sci-fi",
+       },
+
+
+          { 
+       	title : "Moonlight",
+       	director : "Barry Jenkins",
+       	img : "images/moonlight.jpg",
+       	genre : "Drama",
+       },
+
+
+           { 
+       	title : "GoodFellas",
+       	director : "Martin Scorsese",
+       	img : "images/GoodFellas.jpg",
+       	genre : "Classic",
+       },
+
+          { 
+       	title : "Arrival",
+       	director : "Denis Villeneuve",
+       	img : "images/Arrival.jpg",
+       	genre : "Sci-fi",
+       },
+
+ //          { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+ //          { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+ // { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+ //           { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+
+ //          { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+ //          { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+
+ //           { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       }, 
+
+ //         { 
+ //       	title : title,
+ //       	director : director,
+ //       	img : img,
+ //       	genre : genre,
+ //       },
+
+];
--- HelloWorld.js
+++ HelloWorld.js
@@ -1,5 +1,23 @@
-var obj = { 
-    
- 
-}
- 
+	var show = [];
+	
+	function displayMovies() {
+    var show = [];
+   for (var i = 0; i < movies.length; i++){
+      
+			      var storeMovies = $('<div class="poster"></div>');
+			         storeMovies.append('<img src="./'+movies[i].img+'">')
+
+			       $('#allMovies').append(storeMovies);
+			       $('#'+movies[i].genre).append(storeMovies)
+
+			   
+      for (var key in movies){
+      	show.push(movies[i].genre)
+      }
+   
+   }
+     
+     }   
+  displayMovies(); 
+
+
